ea194d9a06ec06f5bb5a3ed228ed3090d0d64895
Rengoan <91058472+Rengoan@users.noreply.github.com>
2021-11-10 11:11:14 +0100

    12

--- ProyectoJuegos/src/com/ceep/juegos/negocio/CatalogoJuegos.java
+++ ProyectoJuegos/src/com/ceep/juegos/negocio/CatalogoJuegos.java
@@ -1,4 +1,3 @@
-
 package com.ceep.juegos.negocio;
 
 import com.ceep.juegos.datos.AccesoDatos;
@@ -15,128 +14,116 @@ import java.io.IOException;
 import java.util.*;
 
 public class CatalogoJuegos implements IACatalogoJuegos {
-    
-    private  final IAccesoDatos datos; //Se utiliza para acceder a la capa de datos
 
-    
-    
+    private final IAccesoDatos datos; //Se utiliza para acceder a la capa de datos
+
     public CatalogoJuegos() {
         this.datos = new AccesoDatos();
     }
 
-
     @Override
     public double CalcularTotalPrecio(String nombreArchivo) throws LecturaDatosEx {
-        File archivo = new File(nombreArchivo);
-//        Juego juegoN = null;
-         double total = 0.0;
-        String[] juego = new String[5]; //(id, Titulo,tipo,precio,plataforma,anio,cantidad)
-//        List<Juego> juegos = new ArrayList<>();
+
+        List<Juego> juegos = new ArrayList<>();
+        double total = 0.0;
 
         try {
+            juegos = this.datos.listar(nombreArchivo);
 
-            BufferedReader entrada = new BufferedReader(new FileReader(archivo));// entrada es el descriptor de lectura
-            String lectura = entrada.readLine();// nos devuelve una línea de nuestro archivo
-            //Lectura = "id";"titulo";"tipo";"precio";"plataforma";"anio";
-            while ((lectura = entrada.readLine()) != null) { // hasta null
-                juego = lectura.split(";"); //Lectura = "id";"titulo";"tipo";"precio";"plataforma";"anio";"cantidad";
-                total += Double.parseDouble(juego[3]);
-                // Creamos un objeto de Juego con cada línea del archivo
-                // Añado cada juego a mi listado de juegos
-//                juegoN = new Juego(Integer.parseInt(juego[0]), juego[1],
-//                        juego[2], Double.parseDouble(juego[3]),
-//                        juego[4], formatoFecha.parse(juego[5]));
-//                juegos.add(juegoN);
-                // Avanzamos en la lectura
-                lectura = entrada.readLine();
+            for (int i = 0; i < juegos.size(); i++) {
+                total += juegos.get(i).getPrecio();
             }
-            entrada.close();
 
-        } catch (FileNotFoundException e) {
-            e.printStackTrace(System.out);
+        } catch (LecturaDatosEx ex) {
+            ex.printStackTrace(System.out);
             throw new LecturaDatosEx("Error de lectura listando los juegos");
-        } catch (IOException e) {
-            e.printStackTrace(System.out);
-            throw new LecturaDatosEx("Error de lectura listando los juegos");
-        } 
+
+        }
         return total;
     }
 
     @Override
     public int contadorJuegos(String nombreArchivo) throws LecturaDatosEx {
-        File archivo = new File(nombreArchivo);
-//        Juego juegoN = null;
-         int total = 0;
-//        String[] juego = new String[5]; //(id, Titulo,tipo,precio,plataforma,anio,cantidad)
-//        List<Juego> juegos = new ArrayList<>();
+        List<Juego> juegos = new ArrayList<>();
+        int contador = 0;
 
         try {
+            juegos = this.datos.listar(nombreArchivo);
 
-            BufferedReader entrada = new BufferedReader(new FileReader(archivo));// entrada es el descriptor de lectura
-            String lectura = entrada.readLine();// nos devuelve una línea de nuestro archivo
-            //Lectura = "id";"titulo";"tipo";"precio";"plataforma";"anio";
-            while ((lectura = entrada.readLine()) != null) { // hasta null
-//                juego = lectura.split(";"); //Lectura = "id";"titulo";"tipo";"precio";"plataforma";"anio";"cantidad";
-                total += 1;
-                // Creamos un objeto de Juego con cada línea del archivo
-                // Añado cada juego a mi listado de juegos
-//                juegoN = new Juego(Integer.parseInt(juego[0]), juego[1],
-//                        juego[2], Double.parseDouble(juego[3]),
-//                        juego[4], formatoFecha.parse(juego[5]));
-//                juegos.add(juegoN);
-                // Avanzamos en la lectura
-                lectura = entrada.readLine();
-            }
-            entrada.close();
+        } catch (LecturaDatosEx ex) {
+            ex.printStackTrace(System.out);
+            throw new LecturaDatosEx("Error de lectura listando los juegos");
 
-        } catch (FileNotFoundException e) {
-            e.printStackTrace(System.out);
-            throw new LecturaDatosEx("Error de lectura listando los juegos");
-        } catch (IOException e) {
-            e.printStackTrace(System.out);
-            throw new LecturaDatosEx("Error de lectura listando los juegos");
-        } 
-        return total;
+        }
+        return juegos.size();
     }
 
     @Override
-    public double maxPrecioJuego(String nombreArchivo)throws LecturaDatosEx {
-         File archivo = new File(nombreArchivo);
-//        Juego juegoN = null;
-         double max = 0.0;
-        String[] juego = new String[5]; //(id, Titulo,tipo,precio,plataforma,anio,cantidad)
-//        List<Juego> juegos = new ArrayList<>();
+    public double maxPrecioJuego(String nombreArchivo) throws LecturaDatosEx {
+        List<Juego> juegos = new ArrayList<>();
+        double max = 0.0;
 
         try {
+            juegos = this.datos.listar(nombreArchivo);
 
-            BufferedReader entrada = new BufferedReader(new FileReader(archivo));// entrada es el descriptor de lectura
-            String lectura= null;
-            //Lectura = "id";"titulo";"tipo";"precio";"plataforma";"anio";
-            while ((lectura = entrada.readLine()) != null) { // hasta null
-                juego = lectura.split(";"); //Lectura = "id";"titulo";"tipo";"precio";"plataforma";"anio";"cantidad";
-                // Creamos un objeto de Juego con cada línea del archivo
-                // Añado cada juego a mi listado de juegos
-                if (Double.parseDouble(juego[3]) >max) {
-                    max = Double.parseDouble(juego[3]);
-                    
+            for (int i = 0; i < juegos.size(); i++) {
+                if (max < juegos.get(i).getPrecio()) {
+                    max = juegos.get(i).getPrecio();
                 }
-//                juegoN = new Juego(Integer.parseInt(juego[0]), juego[1],
-//                        juego[2], Double.parseDouble(juego[3]),
-//                        juego[4], formatoFecha.parse(juego[5]));
-//                juegos.add(juegoN);
-                // Avanzamos en la lectura
-//                lectura = entrada.readLine();
             }
-            entrada.close();
 
-        } catch (FileNotFoundException e) {
-            e.printStackTrace(System.out);
+        } catch (LecturaDatosEx ex) {
+            ex.printStackTrace(System.out);
             throw new LecturaDatosEx("Error de lectura listando los juegos");
-        } catch (IOException e) {
-            e.printStackTrace(System.out);
-            throw new LecturaDatosEx("Error de lectura listando los juegos");
-        } 
+
+        }
         return max;
     }
-    
+
+    @Override
+    public String inicializarCatalogo(String nombreArchivo) {
+        if (this.datos.existe(nombreArchivo)) {
+            this.datos.borrar(nombreArchivo);
+            
+        }
+        return "Catalogo inicializado";
+    }
+
+    @Override
+    public String listarArticulos(String nombreArchivo) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void agregarJuego(String nombreArchivo, Juego juego) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String buscarJuego(String nombreArchivo, String titulo) {
+        try{
+            this.datos.buscar(nombreArchivo, titulo);
+        } catch(LecturaDatosEx ex){
+            ex.printStackTrace(System.out);
+            System.out.println("Excepcion leyendo el archivo");
+        }
+        return "";
+    }
+
+    @Override
+    public String borrarCatalogo(String nombreArchivo) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String borrarJuego(String nombreArchivo,String nombreJuego) {
+        try{
+            this.datos.borrarJuego(nombreArchivo, nombreJuego);
+        } catch(AccesoDatosEx ex){
+            ex.printStackTrace(System.out);
+            System.out.println("Excepcion al borrar un juego");
+        }
+        return "";
+    }
+
 }
--- ProyectoJuegos/src/com/ceep/juegos/negocio/IACatalogoJuegos.java
+++ ProyectoJuegos/src/com/ceep/juegos/negocio/IACatalogoJuegos.java
@@ -1,13 +1,27 @@
 package com.ceep.juegos.negocio;
 
+import com.ceep.juegos.dominio.Juego;
 import com.ceep.juegos.excepciones.LecturaDatosEx;
 
 public interface IACatalogoJuegos {
     
+    //Pre-condicion: No existe el fichero
+    //Post-condicion: Crear un nuevo recurso
+    String inicializarCatalogo(String nombreArchivo);
+    
+    String listarArticulos(String nombreArchivo);
+    
+    void agregarJuego(String nombreArchivo, Juego juego);
+    
+    String buscarJuego(String nombreArchivo, String titulo);
     
     double CalcularTotalPrecio(String nombreArchivo) throws LecturaDatosEx;
     
     int contadorJuegos(String nombreArchivo)throws LecturaDatosEx;
     
     double maxPrecioJuego(String nombreArchivo)throws LecturaDatosEx;
+    
+    String borrarCatalogo(String nombreArchivo);
+    
+    String borrarJuego(String nombreArchivo,String nombreJuego);
 }
